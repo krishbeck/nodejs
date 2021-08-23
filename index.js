d6dfe139e23e5e67a4f7d16287a3d32f949ac85f
Krishna Yadav <krishbeck@yahoo.com>
2021-08-22 20:42:04 -0500

    Add url handling based on url-string
     - Send welcome messaging.
      - 200 and welcome message, if url-string is expected
      - 404, if not
     - Added nodemon to auto restart server when changes are made

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const http = require('http');
+const url = require('url')
 
 const requestListener = (req, res) => {
-    res.writeHead(200);
-    res.end('Hello, World!');
+    const urlString = req.url;
+
+    if (urlString === "/") {                                       // for /, print Hello World
+        res.writeHead(200, {'Content-Type': "text/html"});
+        res.end("Hello World");
+    } else if (urlString === "/welcome") {                         // for /welcome, print Welcome World
+        res.writeHead(200, {'Content-Type': "text/html"});
+        res.end("Welcome World");
+    } else if (urlString.startsWith('/welcome')) {                // starts with /welcome
+        const queryParam = url.parse(req.url, true).query;
+        const name = queryParam.name;
+        res.writeHead(200, {'Content-Type': "text/html"});
+        if (name === undefined || name === "") {                 // for /welcome and name missing, print Welcome Unknown
+            res.end(`Welcome Unknown`);
+        } else {                                                // for /welcome and name present, print Welcome name
+            res.end(`Hello ${name}`);
+        }
+    } else {                                                     // for all other routes, 404
+        res.writeHead(404, {'Content-Type': "text/html"})
+        res.end(JSON.stringify({message: "404 NOT FOUND"}))
+    }
 }
 
+//Create a server using http module
 const server = http.createServer(requestListener);
 server.listen(8080);
